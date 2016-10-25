a68dbb01567d69bd62ee1cba0f26ba54445931c6
shaojung <shaojung@gmail.com>
2016-10-25 20:49:10 +0800

    use Checkbox and RadioGroup

--- app/src/main/java/com/example/yvtc/yvn102502/MainActivity.java
+++ app/src/main/java/com/example/yvtc/yvn102502/MainActivity.java
@@ -1,13 +1,47 @@
 package com.example.yvtc.yvn102502;
 
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.RadioGroup;
 
 public class MainActivity extends AppCompatActivity {
-
+    CheckBox chk;
+    RadioGroup rg;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        chk = (CheckBox) findViewById(R.id.checkBox);
+        rg = (RadioGroup) findViewById(R.id.radioGroup);
+        chk.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked)
+                {
+                    Log.d("MYLOG", "有勾");
+                }
+                else
+                {
+                    Log.d("MYLOG", "沒勾");
+                }
+            }
+        });
+        rg.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                if (checkedId == R.id.radioButton4)
+                {
+                    Log.d("MYLOG", "第一個");
+                }
+                if (checkedId == R.id.radioButton5)
+                {
+                    Log.d("MYLOG", "第二個");
+                }
+            }
+        });
     }
 }
